94bdb8c64dfedd213eda899e90e1aa2643a4d8e0
Jean-Christophe Décary <j-c9@hotmail.com>
2019-08-28 06:33:51 -0400

    V1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# copydeck-CLI
-Command line interfaces for using copydeck module
+# Copydeck-CLI
+Copydeck-CLI is a command line interface that allow you to transform Android, IOS or i18Next ressources files into a more user friendly Csv files. The translation from any type of file to any other type of file.
+## Install
+To install this command line interfaces you will need to run those two command
+
+First of course 
+```
+npm i copydeck-cli
+```
+then 
+```
+npm link @helsing45/copydeck-cli
+```
+
+# Commands
+## Init
+To create your configuration file you will need to run the command
+```
+copydeck --init [path]
+```
+or 
+```
+copydeck -i [path]
+```
+
+Where the path is where your config file will be save.
+
+When creating your config file you will need to answers a few question.
+### 1) Reading file type 
+Determine what type of file you will translate from. You can choose between those 5 types; _'Android'_,_'Csv'_, _'IOS'_, _'i18Next'_, _'universal'_
+### 2) Reading file path
+The path where the reading file are.
+### 3) Filter by
+The eval string use to filter what item you want to keep. If you translate from a Csv file you can filter by column name. Let's say in my Csv file there's a column name Target where you can have the string for Android, IOS, Mobile (use by both Android and IOS) and some for the WebConsole. Now i'm building my config file to translate Csv to Android i can filter with:
+```
+Target == "Android" || Target == "Mobile" 
+```
+
+if in my Csv file contain mutliple project you can always add a column Project and filter like this:
+```
+(Target == "Android" || Target == "Mobile") && (Project == "P1" || Project == "All")
+```
+### 4) Writing file type 
+Determine what type of file you will translate to. You can choose between those 5 types; _'Android'_,_'Csv'_, _'IOS'_, _'i18Next'_, _'universal'_
+
+### 5) Writing file path  
+The path where the file will be written.
+### 6) Is there a default language 
+In the case you translating a Csv to Android you want to indicate what language will be name values 
+### 6.1) What is the default language 
+Indicate what the default language is.
+
+## Translate
+To create your configuration file you will need to run the command
+```
+copydeck --translate_path [path]
+```
+or 
+```
+copydeck -t [path]
+```
+
+Where the path is where your config file is saved
+
+If you are at the location of your config file, and the file is named _config.json_ you can also run.
+
+```
+copydeck translate
+```
--- src/cli.js
+++ src/cli.js
@@ -5,20 +5,29 @@ const fs = require('fs');
 
 export async function cli(args) {
     let options = pargeArgsIntoOptions(args);
+
     options = await promptForMissingOptions(options);
     if (options["init"]) {
+        let path = options["init"].split('/');
+        let destinationImmediate = path[path.length - 1];
 
-        let initPath = options["init"];
-        if (!fs.existsSync(initPath)) {
-            fs.mkdirSync(initPath);
+        if (!destinationImmediate.endsWith('.json')) {
+            path.push("config.json");
         }
-        if (fs.lstatSync(initPath).isDirectory()) {
-            initPath += "/config.json";
-        }
+
         delete options["init"];
+        let configFilePath = path.join('/');
+        path.pop();
+
+        let configParentPath = path.join('/');
+        if (!fs.existsSync(configParentPath)) {
+            fs.mkdirSync(configParentPath);
+        }
         let contents = JSON.stringify(options);
-        fs.writeFileSync(initPath, contents);
-    }else{
+        console.log("Writing " + contents + " file into: " + configFilePath);
+
+        fs.writeFileSync(configFilePath, contents);
+    } else {
         copydeck.translateToFile(options);
     }
 }
@@ -26,19 +35,22 @@ export async function cli(args) {
 function pargeArgsIntoOptions(rawArgs) {
     const args = arg({
         '--init': String,
-        '--path': String,
+        '--translate_path': String,
         '-i': '--init',
-        '-p': '--path'
+        '-t': '--translate_path'
     }, {
         argv: rawArgs.slice(2),
+        stopAtPositional: true
     });
+
     if (args["--init"]) {
         return {
             "init": args["--init"]
         };
     }
-    if (args["--path"]) {
-        let filename = args["--path"];
+
+    if (args["--translate_path"]) {
+        let filename = args["--translate_path"];
         try {
             let data = fs.readFileSync(filename, "utf8");
             return JSON.parse(data);
@@ -46,7 +58,14 @@ function pargeArgsIntoOptions(rawArgs) {
             console.log(error);
         }
     }
-    
+
+    let runTask = args['_'][0];
+    if (runTask == "translate") {
+        let data = fs.readFileSync("config.json", "utf8");
+        return JSON.parse(data);
+    }
+
+
     return {};
 }
 
@@ -57,7 +76,7 @@ async function promptForMissingOptions(options) {
         questions.push({
             type: 'list',
             name: 'from',
-            message: 'Please choose which reading file type',
+            message: 'Reading file type',
             choices: ['Android', 'Csv', 'IOS', 'i18Next', 'universal'],
             default: 'Csv',
         });
@@ -83,7 +102,7 @@ async function promptForMissingOptions(options) {
         questions.push({
             type: 'list',
             name: 'to',
-            message: 'Please choose which writing file type',
+            message: 'Writing file type',
             choices: ['Android', 'Csv', 'IOS', 'i18Next', 'universal'],
             default: 'universal',
         });
@@ -98,7 +117,7 @@ async function promptForMissingOptions(options) {
     }
 
     const answers = await inquirer.prompt(questions);
-    return {
+    options = {
         ...options,
         "from": options.from || answers.from,
         "to": options.to || answers.to,
@@ -106,4 +125,32 @@ async function promptForMissingOptions(options) {
         "writePath": options.writePath || answers.writePath,
         "filter": options.filter || answers.filter
     }
+
+    if (typeof options.defaultLanguageRequired == 'undefined') {
+
+        let answer = await inquirer.prompt({
+            type: 'confirm',
+            name: 'defaultLanguageRequired',
+            message: 'Is there a default language ?'
+        });
+        options = {
+            ...options,
+            "defaultLanguageRequired": answer.defaultLanguageRequired,
+        }
+    }
+
+    if (options.defaultLanguageRequired && !options.defaultLang) {
+
+        let answer = await inquirer.prompt({
+            type: 'input',
+            name: 'defaultLang',
+            message: 'What is the default language?'
+        });
+        options = {
+            ...options,
+            "defaultLang": answer.defaultLang,
+        }
+    }
+
+    return options;
 }
